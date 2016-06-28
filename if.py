40200d0eaf8f9640e3b523ef86e705a1f534b555
Pieter de Bie <pieter.de.bie@planet.nl>
2016-06-28 16:10:23 +0200

    Dit zijn de eerste probeerselen

---
+++ if.py
@@ -0,0 +1,10 @@
+if x < y :
+    print 'x is less than y'
+elif x > y :
+    print 'x is greater than y'
+else:
+    print 'x and y are equal'
+
+for i in range(10):
+    x = random.random()
+    print x
---
+++ test.py
@@ -0,0 +1,8 @@
+namelist = ["Luanne Longo", "Latesha Donelson", "Giovanni Goya", "Lise Brannen",
+"Alvera Rowden", "Zaida Braziel", "Charise Bartkowiak", "Debi Petre", "Kimi Califano", 
+"Tamesha Quin", "Joane Mower", "Shanell Aubry", "Tana Bonet",
+"Madeline Messerly", "Anabel Smalley", "Shenna Yap", "Ema Thorsen", "Kazuko Lesure", 
+"Obdulia Buhler", "Tiera Kendricks"]
+
+for name in namelist:
+	print name
